93829ef435692f72d6492bcd76de48a63daef711
eshamster <hamgoostar@gmail.com>
2018-01-20 18:14:21 +0000

    Refactor by moving WS client management to server.lisp

--- src/server.lisp
+++ src/server.lisp
@@ -2,14 +2,13 @@
   (:use :cl)
   (:export :start
            :stop
-
-           :server-started-p
-           :get-server-port
-           :add-server-stop-hook)
+           :server-started-p)
   (:import-from :sample-cl-web-socket.static-server
                 :*static-app*)
   (:import-from :sample-cl-web-socket.ws-server
-                :*ws-app*))
+                :*ws-app*
+                :start-ws-client
+                :stop-ws-client))
 (in-package :sample-cl-web-socket.server)
 
 (defparameter *mv*
@@ -26,26 +25,18 @@
 (defun server-started-p ()
   (not (null *server*)))
 
-(defun get-server-port ()
-  *port*)
-
 (defun start (&key (port 5000))
   (stop)
   (setf *port* port
         *server*
         (clack:clackup
          (lack:builder *mv* *static-app*)
-         :port port)))
-
-(defvar *server-stop-hooks* nil)
-
-(defun add-server-stop-hook (callback)
-  (pushnew callback *server-stop-hooks*))
+         :port port))
+  (start-ws-client :port port))
 
 (defun stop ()
   (when *server*
-    (dolist (hook *server-stop-hooks*)
-      (funcall hook))
+    (stop-ws-client)
     (clack:stop *server*)
     (setf *server* nil
           *port* nil)))
--- src/utils.lisp
+++ src/utils.lisp
@@ -1,28 +1,11 @@
 (defpackage sample-cl-web-socket.utils
   (:use :cl)
   (:import-from :sample-cl-web-socket.ws-server
-                :start-ws-client
-                :stop-ws-client
                 :ws-client-started-p
-                :send-from-client)
-  (:import-from :sample-cl-web-socket.server
-                :server-started-p
-                :get-server-port
-                :add-server-stop-hook))
+                :send-from-client))
 (in-package :sample-cl-web-socket.utils)
 
-(add-server-stop-hook
- (lambda ()
-   (when (ws-client-started-p)
-     (stop-ws-client))))
-
-(defun try-to-start-client ()
-  (when (server-started-p)
-    (start-ws-client :port (get-server-port))))
-
 (defun send-ps-code (body)
-  (unless (ws-client-started-p)
-    (try-to-start-client))
   (when (ws-client-started-p)
     (send-from-client (format nil "~W" `(progn ,@body)))))
 
